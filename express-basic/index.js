7c6f3f05b2b7b4ee760dc6cb180cfcc4f5025f44
talentceffort <talentceffort@gmail.com>
2020-03-11 22:13:21 +0900

    사용자 미들웨어 작성

--- express-basic/index.js
+++ express-basic/index.js
@@ -3,19 +3,20 @@ const app = express();
 
 // 미들웨어는 함수들의 연속
 
-// 미들웨어를 추가해 보자!!
-function logger(req, res, next) {
-  console.log('i am logger')
-  next();
+
+function commonmw (req, res, next) {
+  console.log('commonmw')
+  next(new Error('error ouccered'))
 }
 
-function logger2(req, res, next) {
-  console.log(`i am logger2`);
-  next();
+function errormw(err, req, res, next) {
+  console.log(err.message)
+  // 에러 처리
+  next()
 }
 
-app.use(logger)
-app.use(logger2)
+app.use(commonmw)
+app.use(errormw)
 
 app.listen(3500, function() {
   console.log('Server is running');
