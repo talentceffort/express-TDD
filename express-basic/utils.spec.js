21c59f9ed59ddb2d357ba11176da121ee12b56b1
talentceffort <talentceffort@gmail.com>
2020-03-14 18:15:29 +0900

    assert 대신 should 사용 해보기

--- express-basic/utils.spec.js
+++ express-basic/utils.spec.js
@@ -1,9 +1,11 @@
 const utils = require('./utils')
-const assert = require('assert')
+// const assert = require('assert') Node 공식 문서에서는 노드 자체의 assert 사용을 권장하지 않음.
+const should = require('should')
 
 describe('utils.js 모듈의 capitalize() 함수는', () => {
   it('문자열의 첫번째 문자를 대문자로 변환한다.', () => {
     const result = utils.capitialize('hello')
-    assert.equal(result, 'Hello')
+    // result.equal(result, 'Hello')
+    result.should.be.equal('Hello')
   })
 })
